568bba37b9d1c60d5328df185667bd8c391a929f
Juan Lavigna <juanlavigna@gmail.com>
2022-07-23 19:53:30 -0300

    Make a new grid from prompt and delete the old one

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const CONTAINER = document.querySelector(".container")
-for(let i = 0; i < 16; i++){
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("square");
-    CONTAINER.appendChild(newDiv);
-    newDiv.addEventListener("mouseover", draw(newDiv))
+const CONTAINER = document.querySelector(".container");
+const BUTTON_SIZE = document.querySelector("#size");
+let gridSize = 256;
+const gridPx = 500;
+
+makeGrid(gridSize)
+
+function makeGrid(size){
+    for(let i = 0; i < size; i++){
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("square");
+        CONTAINER.appendChild(newDiv);
+        newDiv.addEventListener("mouseover", draw(newDiv));
+    }
 }
 
 function draw(newDiv) {
     return function () {
         newDiv.style.backgroundColor = "black";
-    };
+    };  
 }
+
+BUTTON_SIZE.addEventListener("click", function(){
+    removeChilds();
+    gridSize = prompt("Ingrese tamaño de la cuadricula");
+    makeGrid(gridSize);
+})
+
+function removeChilds (){
+    while (CONTAINER.firstChild){
+        CONTAINER.removeChild(CONTAINER.firstChild);
+    }
+}
+
 
