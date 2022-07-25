3bbb8ed60b8e20430df318222cd1dc63838a2b42
Juan Lavigna <juanlavigna@gmail.com>
2022-07-25 11:02:22 -0300

    Set squares per side in the same grid size

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const CONTAINER = document.querySelector(".container");
 const BUTTON_SIZE = document.querySelector("#size");
-let gridSize = 256;
+const SQUARES = document.querySelectorAll(".container>div");
+let gridSize = 16;
 const gridPx = 500;
 
 makeGrid(gridSize)
 
+function resize (size, square){
+    let divPx = gridPx/gridSize
+    for(let i = 0; i<size; i++){
+        square.style.width = `${divPx}px`;
+        square.style.height = `${divPx}px`;
+    }
+}
+
 function makeGrid(size){
+    size = Math.pow(size, 2)
     for(let i = 0; i < size; i++){
         let newDiv = document.createElement("div");
         newDiv.classList.add("square");
         CONTAINER.appendChild(newDiv);
+        resize(size, newDiv);
         newDiv.addEventListener("mouseover", draw(newDiv));
     }
 }
@@ -20,16 +31,16 @@ function draw(newDiv) {
     };  
 }
 
-BUTTON_SIZE.addEventListener("click", function(){
-    removeChilds();
-    gridSize = prompt("Ingrese tamaño de la cuadricula");
-    makeGrid(gridSize);
-})
-
 function removeChilds (){
     while (CONTAINER.firstChild){
         CONTAINER.removeChild(CONTAINER.firstChild);
     }
 }
 
+
+BUTTON_SIZE.addEventListener("click", function(){
+    removeChilds();
+    gridSize = prompt("Ingrese cantidad de cuadrados por lado");
+    makeGrid(gridSize);
+})
 
